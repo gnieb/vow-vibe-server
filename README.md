76b357075043665ed2c0b3191b5a24c1b323d8e3
Grace Nieboer <grace.nieboer@outlook.com>
2023-08-17 22:46:14 -0400

    add bcrypt and create user model

---
+++ README.md
@@ -0,0 +1,12 @@
+# Vow Vibe Server
+## This is the backend server for Vow Vibe application. An app to help get anyone to the big day. 
+
+MODELS:
+
+USER
+To do's 
+
+
+guest list
+
+calendar
--- config.py
+++ config.py
@@ -1,6 +1,13 @@
 from flask import Flask;
 from flask_restful import Api
+from flask_sqlalchemy import SQLAlchemy
+from flask_migrate import Migrate
 
 app = Flask(__name__)
 api = Api(app)
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///app.db'
+db = SQLAlchemy()
+migrate = Migrate(app, db)
+db.init_app(app)
+
 
