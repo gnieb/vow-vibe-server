921c9304f3fc0553baeccb0f60529e59ff89f50c
Grace Nieboer <grace.nieboer@outlook.com>
2023-10-05 14:39:13 -0400

    config app to connect to new postgres database and create first two tables

--- config.py
+++ config.py
@@ -2,10 +2,12 @@ from flask import Flask;
 from flask_restful import Api
 from flask_sqlalchemy import SQLAlchemy
 from flask_migrate import Migrate
+import os
 
 app = Flask(__name__)
 api = Api(app)
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///app.db'
+app.config['SQLALCHEMY_DATABASE_URI'] = os.environ['DATABASE_URI']
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 db = SQLAlchemy()
 migrate = Migrate(app, db)
 db.init_app(app)
