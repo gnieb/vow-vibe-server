5781b173b167064ced242683cf2a98534ef101ff
Grace Nieboer <grace.nieboer@outlook.com>
2023-10-07 11:43:00 -0400

    add seed to do data for user 1

--- seed.py
+++ seed.py
@@ -1,7 +1,7 @@
 from app import app
 from config import db
 
-from models import User, Wedding, Guest
+from models import User, Wedding, Guest, ToDo
 
 def seedData():
     print("deleting old data")
@@ -13,10 +13,15 @@ def seedData():
 # using an unencrypted password hash just for testing purposes
     # u1 = User(email="grace@gmail.com", first_name="grace", last_name="nieboer", _password_hash="dsgahk3w980!klf", )
     # w1 = Wedding(date="November, 2024", user_id=1)
-    g1 = Guest(first_name="Johanna", last_name="Nieboer", wedding_id=1)
-    g2 = Guest(first_name="Matthew", last_name="Nieboer", wedding_id=1)
-    g3 = Guest(first_name="Jack", last_name="Nieboer", wedding_id=1)
-    db.session.add_all([g1, g2, g3])
+    # g1 = Guest(first_name="Johanna", last_name="Nieboer", wedding_id=1)
+    # g2 = Guest(first_name="Matthew", last_name="Nieboer", wedding_id=1)
+    # g3 = Guest(first_name="Jack", last_name="Nieboer", wedding_id=1)
+    t1 = ToDo(todo="book a venue", isDone=False, user_id=1)
+    t2 = ToDo(todo="research photographers", isDone=False, user_id=1)
+    t3 = ToDo(todo="thrift a Justin Alexander wedding dress", isDone=False, user_id=1)
+
+
+    db.session.add_all([t1, t2, t3])
     db.session.commit()
 
     print("Seeding complete")
